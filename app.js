75d412bd5c21e82a92163c239bba521f4442dbdf
Guilherme Pontes <guilhermepontes@msn.com>
2014-01-04 14:47:36 -0200

    adding use strict, adding public and private methods and invokating the app

--- app.js
+++ app.js
@@ -8,6 +8,16 @@
  * Notes        : Copy and reproduce as much as you want.
 ---------------------------------------------------------------------------*/
 
+/*--------------------------------------------------------------------------
+ * ModulePattern Bootstrap
+ *
+ * Created      : 21/01/2013
+ * Modified     : 04/01/2015
+ * Version      : 2.0
+ * UI Developer : Guilherme Pontes
+ * Notes        : Copy and reproduce as much as you want.
+---------------------------------------------------------------------------*/
+
 /**
 * This is MyApp, it makes something awesome.
 * It's an self invokated function that runs automatically/immediately.
@@ -16,6 +26,39 @@
 * @constructor
 */
 var MyApp = window.MyApp || (function(document, window){
-	
+	"use strict";
+
+	var app, _private;
+
+	/**
+	 * The _private variable encapsulate all PRIVATE functions 
+	 * and properties.
+	 * 
+	 * @property _private
+	 * @type {Object}
+	 * @default {Object}
+	 */
+	_private = {
+	};
+
+	/**
+	 * The _app variable encapsulate all PUBLIC functions
+	 * and properties.
+	 * 
+	 * @property _private 
+	 * @type {Object} 
+	 * @default {Object} 
+	 */
+	var app = {
+	};
+
+	// make app visible by the MyApp;
+	return app;
 
 })(document, window);
+
+
+// another self invokated function to executes the app.
+(function(){
+	MyApp.init();
+})();
