482265a775ccad45d19212a6652bb15efa51ffc6
Guilherme Pontes <guilhermepontes@msn.com>
2014-01-04 15:01:17 -0200

    adding tests

---
+++ tests/spec/AppSpec.js
@@ -0,0 +1,40 @@
+describe("Scope Tests", function() {
+  it("MyApp must exists", function() {
+    expect(MyApp).toBeDefined();
+  });
+
+  describe("Public methods", function(){
+  	it("MyApp.init() must be PUBLIC", function() {
+	    expect(MyApp.init).toBeDefined();
+	  });
+
+	  it("MyApp._cache() must be PUBLIC", function() {
+	    expect(MyApp._cache).toBeDefined();
+	  });
+
+	  it("MyApp._bind() must be PUBLIC", function() {
+	    expect(MyApp._bind).toBeDefined();
+	  });
+  });
+
+  describe("Public properties", function(){
+  	it("MyApp.body must be PUBLIC", function() {
+	    expect(MyApp.body).toBeDefined();
+	  });
+
+	  it("MyApp.makeMeRed must be PUBLIC", function() {
+	    expect(MyApp.makeMeRed).toBeDefined();
+	  });
+  });
+
+  describe("Private methods", function(){
+  	it("MyApp.app must PRIVATE", function() {
+	    expect(MyApp.app).toBeUndefined();
+	  });
+
+	  it("MyApp._private must be PRIVATE", function() {
+	    expect(MyApp._private).toBeUndefined();
+	  });
+  });
+  
+});
