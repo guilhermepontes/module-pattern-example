c6bdf8eef4e0cec109df73ca7c44d9a91d124e9a
Guilherme Pontes <guilhermepontes@msn.com>
2014-01-04 14:46:32 -0200

    adding MyApp class, documented with YUI DOC pattern

--- app.js
+++ app.js
@@ -7,3 +7,15 @@
  * UI Developer : Guilherme Pontes
  * Notes        : Copy and reproduce as much as you want.
 ---------------------------------------------------------------------------*/
+
+/**
+* This is MyApp, it makes something awesome.
+* It's an self invokated function that runs automatically/immediately.
+*
+* @class MyApp
+* @constructor
+*/
+var MyApp = window.MyApp || (function(document, window){
+	
+
+})(document, window);
