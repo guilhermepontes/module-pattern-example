940ba1cd2227b8029cee45be3a3895d597147cf1
Guilherme Pontes <guisantos@sp.r7.com>
2014-08-27 10:46:54 -0300

    removing useless file and fixing indentation :grin:

--- app.js
+++ app.js
@@ -16,142 +16,142 @@
 * @constructor
 */
 var MyApp = window.MyApp || (function(document, window){
-	"use strict";
+  "use strict";
 
-	var app, _private;
+  var app, _private;
 
-	/**
-	 * The _private variable encapsulate all PRIVATE functions 
-	 * and properties.
-	 * 
-	 * @property _private
-	 * @type {Object}
-	 * @default {Object}
-	 */
-	_private = {
-		/**
-		 * The _config variable sets all properties that will be 
-		 * handled by the app.
-		 * 
-		 * @property _private
-		 * @type {Object}
-		 * @default {Object}
-		 */
-		_config: {
-			/**
-			 * Background options
-			 * 
-			 * @property background
-			 * @type {Object}
-			 * @default {Object}
-			 */
-			background: {
-				/**
-				 * Background color option
-				 * 
-				 * @property color
-				 * @type {String}
-				 * @default "white"
-				 */
-				color: "white"
-			}
-		},
+  /**
+   * The _private variable encapsulate all PRIVATE functions
+   * and properties.
+   *
+   * @property _private
+   * @type {Object}
+   * @default {Object}
+   */
+  _private = {
+    /**
+     * The _config variable sets all properties that will be
+     * handled by the app.
+     *
+     * @property _private
+     * @type {Object}
+     * @default {Object}
+     */
+    _config: {
+      /**
+       * Background options
+       *
+       * @property background
+       * @type {Object}
+       * @default {Object}
+       */
+      background: {
+        /**
+         * Background color option
+         *
+         * @property color
+         * @type {String}
+         * @default "white"
+         */
+        color: "white"
+      }
+    },
 
-		/**
-		 * Makes the body element from {MyApp.body} the color setted,
-		 * if not setted it will be the default color 
-		 * from {_private._config.background.color}
-		 * 
-		 * @method setBGColor
-		 * @param {String} color A color to be an background color from body.
-		 * @return {Undefined}
-		 */
-		setBGColor: function(color) {
-			app.body.style.background = color || this._config.background.color;
-		},
+    /**
+     * Makes the body element from {MyApp.body} the color setted,
+     * if not setted it will be the default color
+     * from {_private._config.background.color}
+     *
+     * @method setBGColor
+     * @param {String} color A color to be an background color from body.
+     * @return {Undefined}
+     */
+    setBGColor: function(color) {
+      app.body.style.background = color || this._config.background.color;
+    },
 
-		/**
-		 * Makes body blue from {MyApp.init} method.
-		 * 
-		 * @method makeBackgroundBlue
-		 * @return {Undefined}
-		 */
-		makeBackgroundBlue: function() {
-			_private.setBGColor("blue");
-		},
+    /**
+     * Makes body blue from {MyApp.init} method.
+     *
+     * @method makeBackgroundBlue
+     * @return {Undefined}
+     */
+    makeBackgroundBlue: function() {
+      _private.setBGColor("blue");
+    },
 
-		/**
-		 * Makes body red from click event at {MyApp.bind} from 
-		 * {MyApp.makeBodyRed}.
-		 * 
-		 * @method makeBackgroundRed
-		 * @return {Undefined}
-		 */
-		makeBackgroundRed: function() {
-			_private.setBGColor("red");
-		}
-	};
+    /**
+     * Makes body red from click event at {MyApp.bind} from
+     * {MyApp.makeBodyRed}.
+     *
+     * @method makeBackgroundRed
+     * @return {Undefined}
+     */
+    makeBackgroundRed: function() {
+      _private.setBGColor("red");
+    }
+  };
 
-	/**
-	 * The _app variable encapsulate all PUBLIC functions
-	 * and properties.
-	 * 
-	 * @property _private 
-	 * @type {Object} 
-	 * @default {Object} 
-	 */
-	app = {
+  /**
+   * The _app variable encapsulate all PUBLIC functions
+   * and properties.
+   *
+   * @property _private
+   * @type {Object}
+   * @default {Object}
+   */
+  app = {
 
-		/**
-		 * The init method, is the function that initialize the project,  
-		 * by caching variables and binding objects and can initialize
-		 * other methods too.
-		 *
-		 * @method init
-		 * @return {Undefined}
-		*/
-		init: function() {
-			this._cache();
-			this._bind();
+    /**
+     * The init method, is the function that initialize the project,
+     * by caching variables and binding objects and can initialize
+     * other methods too.
+     *
+     * @method init
+     * @return {Undefined}
+    */
+    init: function() {
+      this._cache();
+      this._bind();
 
-			//the background must be blue first..
-			_private.makeBackgroundBlue();
-		},
+      //the background must be blue first..
+      _private.makeBackgroundBlue();
+    },
 
-		/**
-		 * The _cache method creates public properties that will be 
-		 * handled by other methods. Caching elements to not have to be
-		 * called every single time.
-		 * 
-		 *
-		 * @method _cache
-		 * @return {Undefined}
-		*/
-		_cache: function(){
-			this.body = document.body;
-			this.makeMeRed = document.querySelector('.makeBodyRed');
-		},
+    /**
+     * The _cache method creates public properties that will be
+     * handled by other methods. Caching elements to not have to be
+     * called every single time.
+     *
+     *
+     * @method _cache
+     * @return {Undefined}
+    */
+    _cache: function(){
+      this.body = document.body;
+      this.makeMeRed = document.querySelector('.makeBodyRed');
+    },
 
-		/**
-		 * The _bind method bind all cached dom elements from {MyApp._cache}.
-		 * It sets methods to events made by the DOM.
-		 * 
-		 *
-		 * @method _bind
-		 * @return {Undefined}
-		*/
-		_bind: function() {
-			this.makeMeRed.addEventListener('click', _private.makeBackgroundRed , false);
-		}
-	};
+    /**
+     * The _bind method bind all cached dom elements from {MyApp._cache}.
+     * It sets methods to events made by the DOM.
+     *
+     *
+     * @method _bind
+     * @return {Undefined}
+    */
+    _bind: function() {
+      this.makeMeRed.addEventListener('click', _private.makeBackgroundRed , false);
+    }
+  };
 
-	// make app visible by the MyApp;
-	return app;
+  // make app visible by the MyApp;
+  return app;
 
 })(document, window);
 
 
 // another self invokated function to executes the app.
 (function(){
-	MyApp.init();
+  MyApp.init();
 })();
