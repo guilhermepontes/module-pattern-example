a3b4788a3665c87c5c92ed4bb32bfe8c3792ffd6
Guilherme Pontes <guisantos@sp.r7.com>
2014-08-27 11:12:26 -0300

    karma configuration

---
+++ karma.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    exclude: [],
+    preprocessors: {},
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false
+  });
+};
